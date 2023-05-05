101e7e4220be427a0ec2e984b84e2a1a09be8c21
Guilherme Moraes <guimoraes.dev@gmail.com>
2023-05-05 16:03:17 -0300

    feat: create projects card

--- src/components/organisms/ProjectsSection/index.tsx
+++ src/components/organisms/ProjectsSection/index.tsx
@@ -1,3 +1,5 @@
+import { GithubLogo, Link } from "@phosphor-icons/react";
+import Image from "next/image";
 import { ReactNode } from "react";
 
 import { getPinnedRepos } from "@/graphql/queries";
@@ -5,49 +7,56 @@ import { getPinnedRepos } from "@/graphql/queries";
 export const Section = async (): Promise<ReactNode> => {
   const data = await getPinnedRepos();
 
-  console.log({ data });
-
   return (
-    <section className="flex flex-col gap-8 p-16 text-black dark:text-white">
+    <section className="flex w-full flex-col gap-8 p-16 text-black dark:text-white">
       <h2 className="text-2xl font-medium">Best projects</h2>
 
-      <article className="flex flex-col gap-4 ">
-        <p>
-          I&apos;m an experienced front-end developer with a strong passion for
-          delivering innovative, high-quality solutions.
-        </p>
+      <ul className="flex flex-col gap-6">
+        {data?.map((project, index) => (
+          <li
+            key={project.id}
+            className={`flex flex-col gap-3 translate-x-${index * 3}`}
+          >
+            <Image
+              src={`https://raw.githubusercontent.com/GuiMoraesDev/${project.title}/main/public/img/cover.png`}
+              alt={project.title}
+              width={320}
+              height={168}
+              className="h-40 w-72 overflow-hidden rounded-lg border-2 object-cover"
+              style={{ height: "100%" }}
+            />
 
-        <p>
-          My expertise in technologies such as JavaScript, TypeScript, React,
-          and CSS-in-JS allows me to create complex solutions that prioritize
-          user experience.
-        </p>
+            <section className="flex w-full items-center justify-between">
+              <p className="capitalize">{project.title}</p>
 
-        <p>
-          I have worked with small and medium-sized companies, honing my
-          communication skills with all stakeholders involved in the development
-          process.
-        </p>
+              <nav className="flex items-center gap-8">
+                <a href={project.project_link} target="_blank">
+                  <Link size={20} />
+                </a>
 
-        <p>
-          Whether working independently or as part of a team, I am an effective
-          time and resource manager, and I&apos;m comfortable with every aspect
-          of the development process. With a commitment to continuous learning
-          and self-improvement, I stay ahead of the curve in an ever-changing
-          industry.
-        </p>
-      </article>
+                <a href={project.github_link} target="_blank">
+                  <GithubLogo size={20} />
+                </a>
+              </nav>
+            </section>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
 
-const shimmer = `relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_1.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/40 before:to-transparent`;
-
 function Skeleton() {
+  const shimmer = `relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_1.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/40 before:to-transparent`;
+
   return (
     <section className="flex flex-col gap-3">
       <div className={`h-40 w-72 rounded-lg border-2 bg-gray-300 ${shimmer}`} />
-      <div className={`h-4 w-4/6 rounded-md bg-gray-300 ${shimmer}`} />
+
+      <div className="flex items-center justify-between">
+        <span className={`h-4 w-3/6 rounded-md bg-gray-300 ${shimmer}`} />
+        <span className={`h-4 w-2/6 rounded-md bg-gray-300 ${shimmer}`} />
+      </div>
     </section>
   );
 }
