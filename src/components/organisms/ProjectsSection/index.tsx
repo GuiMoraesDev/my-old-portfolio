8313f91621cf9c7903122924f3de11d33c79c2c3
Guilherme Moraes <guimoraes.dev@gmail.com>
2023-05-06 13:42:01 -0300

    fix: fix return typing projects section

--- src/components/organisms/ProjectsSection/index.tsx
+++ src/components/organisms/ProjectsSection/index.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 import { getPinnedRepos } from "@/graphql/queries";
 
-export const Section = async (): Promise<ReactNode> => {
+export const Section = async () => {
   const data = await getPinnedRepos();
 
   return (
